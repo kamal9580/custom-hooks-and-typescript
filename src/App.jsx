8c1a4382f959bfa6cdcbb2eb97c26c1ef9bd2492
kamal9580 <nileshprince3118@gmail.com>
2024-07-30 21:40:06 +0530

    add swr for data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -140,3 +140,22 @@
 // }
 
 // export default App
+
+
+// swr - React Hooks for Data Fetching
+
+// import useSWR from 'swr'
+
+// // const fetcher = (url) => fetch(url).then((res) => res.json());
+// const fetcher = async function(url) {
+//   const data = await fetch(url);
+//   const json = await data.json();
+//   return json;
+// };
+
+// function Profile() {
+//   const { data, error, isLoading } = useSWR('https://sum-server.100xdevs.com/todos', fetcher)
+ 
+//   if (error) return <div>failed to load</div>
+//   if (isLoading) return <div>loading...</div>
+//   return <div>hello, you have {data.todos.length} todos!</div>
