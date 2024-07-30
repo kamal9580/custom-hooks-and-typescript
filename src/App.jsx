9fda86690ed99d6fa11291b6fdcf490614d80d44
kamal9580 <nileshprince3118@gmail.com>
2024-07-30 21:35:38 +0530

    added auto refreshing hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,142 @@
 //Converting the data fetching bit to a custom hook
  
 
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
 
-function useTodos() {
-  const [todos, setTodos] = useState([])
+// function useTodos() {
+//   const [todos, setTodos] = useState([])
 
-  useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todos")
-      .then(res => {
-        setTodos(res.data.todos);
-      })
-  }, [])
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//       })
+//   }, [])
 
-  return todos;
-}
+//   return todos;
+// }
 
-function App() {
-  const todos = useTodos();
+// function App() {
+//   const todos = useTodos();
 
-  return (
-    <>
-      {todos.map(todo => <Track todo={todo} />)}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
 
-function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
-}
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
 
-export default App
+// export default App
+
+// Cleaning the hook to include a loading parameter
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// function useTodos() {
+//   const [loading, setLoading] = useState(true);
+//   const [todos, setTodos] = useState([])
+
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       })
+//   }, [])
+
+//   return {
+//     todos: todos,
+//     loading: loading
+//   };
+// }
+
+// function App() {
+//   const { todos, loading } = useTodos();
+
+//   if (loading) {
+//     return <div>
+//       Loading...
+//     </div>
+//   }
+
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
+
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
+
+// export default App
+
+
+// Auto refreshing hook
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// function useTodos(n) {
+//   const [todos, setTodos] = useState([])
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const value = setInterval(() => {
+//       axios.get("https://sum-server.100xdevs.com/todos")
+//         .then(res => {
+//           setTodos(res.data.todos);
+//           setLoading(false);
+//         })
+//     }, n * 1000)
+  
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       })
+
+//     return () => {
+//       clearInterval(value)
+//     }
+//   }, [n])
+
+//   return {todos, loading};
+// }
+
+// function App() {
+//   const {todos, loading} = useTodos(10);
+
+//   if (loading) {
+//     return <div> loading... </div>
+//   }
+
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
+
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
+
+// export default App
